7e3ed9afa9da828d1c6c871cb3396ada99273ab1
GiovaniFZ <giovanifinazzi@gmail.com>
2024-10-25 12:06:27 -0300

    style: Use red color

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { LocalStorageAdapter } from './LocalStorageAdapter.ts';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme.ts';
 const localStorageAdapter = new LocalStorageAdapter();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
     <App ordersRepository={localStorageAdapter} />
+    </ThemeProvider>
   </StrictMode>,
 )
